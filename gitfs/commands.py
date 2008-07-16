dd1a9450d1d52a3fc9a2560eb8ccfc3cf6da987f
Tommi Virtanen <tv@eagain.net>
2008-07-17 00:48:34 +0300

    Add git calling utility functions.

---
+++ gitfs/commands.py
@@ -0,0 +1,243 @@
+import subprocess
+
+def init_bare(repo):
+    returncode = subprocess.call(
+        args=[
+            'git',
+            '--bare',
+            '--git-dir=%s' % repo,
+            'init',
+            '--quiet',
+            ],
+        close_fds=True,
+        )
+    if returncode != 0:
+        raise RuntimeError('git init failed')
+
+def fast_import(
+    repo,
+    commits,
+    ):
+    """
+    Create an initial commit.
+    """
+    child = subprocess.Popen(
+        args=[
+            'git',
+            '--git-dir=%s' % repo,
+            'fast-import',
+            '--quiet',
+            ],
+        stdin=subprocess.PIPE,
+        close_fds=True,
+        )
+
+    for commit in commits:
+        files = list(commit['files'])
+        for index, filedata in enumerate(files):
+            child.stdin.write("""\
+blob
+mark :%(mark)d
+data %(len)d
+%(content)s
+""" % dict(
+                mark=index+1,
+                len=len(filedata['content']),
+                content=filedata['content'],
+                ))
+        child.stdin.write("""\
+commit refs/heads/master
+author %(author)s %(author_time)s
+committer %(committer)s %(commit_time)s
+data %(commit_msg_len)d
+%(commit_msg)s
+""" % dict(
+                author=commit.get('author', commit['committer']),
+                author_time=commit.get('author_time', commit['commit_time']),
+                committer=commit['committer'],
+                commit_time=commit['commit_time'],
+                commit_msg_len=len(commit['message']),
+                commit_msg=commit['message'],
+                ))
+        parent = commit.get('parent')
+        if parent is not None:
+            assert not parent.startswith(':')
+            child.stdin.write("""\
+from %(parent)s
+""" % dict(
+                    parent=parent,
+                    ))
+        for index, filedata in enumerate(files):
+            child.stdin.write(
+                'M %(mode)s :%(index)d %(path)s\n' % dict(
+                    mode=filedata.get('mode', '100644'),
+                    index=index+1,
+                    path=filedata['path'],
+                    ),
+                )
+
+    child.stdin.close()
+    returncode = child.wait()
+    if returncode != 0:
+        raise RuntimeError(
+            'git fast-import failed', 'exit status %d' % returncode)
+
+def rev_parse(repo, rev):
+    process = subprocess.Popen(
+        args=[
+            'git',
+            '--git-dir=%s' % repo,
+            'rev-parse',
+            '--default',
+            rev,
+            ],
+        close_fds=True,
+        stdout=subprocess.PIPE,
+        )
+    sha = process.stdout.read()
+    returncode = process.wait()
+    if returncode != 0:
+        raise RuntimeError('git rev-parse failed')
+    if not sha:
+        return None
+    sha = sha.rstrip('\n')
+    return sha
+
+def get_symbolic_ref(repo, ref):
+    process = subprocess.Popen(
+        args=[
+            'git',
+            '--git-dir=%s' % repo,
+            'symbolic-ref',
+            ref,
+            ],
+        close_fds=True,
+        stdout=subprocess.PIPE,
+        )
+    value = process.stdout.read()
+    returncode = process.wait()
+    if returncode != 0:
+        raise RuntimeError('git symbolic-ref failed')
+    if not value:
+        return None
+    value = value.rstrip('\n')
+    return value
+
+def ls_tree(
+    repo,
+    path=None,
+    treeish=None,
+    children=None,
+    ):
+    if path is None:
+        path = ''
+    if treeish is None:
+        treeish = 'HEAD'
+    if children is None:
+        children = True
+    assert not path.startswith('/')
+    assert not path.endswith('/')
+    if children:
+        if path:
+            path = path+'/'
+    process = subprocess.Popen(
+        args=[
+            'git',
+            '--git-dir=%s' % repo,
+            'ls-tree',
+            '-z',
+            '--full-name',
+            treeish,
+            '--',
+            path,
+            ],
+        close_fds=True,
+        stdout=subprocess.PIPE,
+        )
+    buf = ''
+    while True:
+        new = process.stdout.read(8192)
+        buf += new
+        while True:
+            try:
+                (entry, buf) = buf.split('\0', 1)
+            except ValueError:
+                break
+            meta, filename = entry.split('\t', 1)
+            mode, type_, object = meta.split(' ', 2)
+            if children:
+                assert filename.startswith(path)
+                basename = filename[len(path):]
+            else:
+                basename = filename
+            yield dict(
+                mode=mode,
+                type=type_,
+                object=object,
+                name=basename,
+                )
+        if not new:
+            break
+    if buf:
+        raise RuntimeError(
+            'git ls-tree output did not end in NUL')
+    returncode = process.wait()
+    if returncode != 0:
+        raise RuntimeError('git ls-tree failed')
+
+def cat_file_by_path(repo, path, type_=None, rev=None):
+    if type_ is None:
+        type_ = 'blob'
+    if rev is None:
+        rev = 'HEAD'
+    for data in ls_tree(
+        repo=repo,
+        path=path,
+        treeish=rev,
+        children=False,
+        ):
+        if data['type'] != type_:
+            raise RuntimeError(
+                'found object, but type does not match: %r' \
+                    % data['type']
+                )
+        process = subprocess.Popen(
+            args=[
+                'git',
+                '--git-dir=%s' % repo,
+                'cat-file',
+                'blob',
+                data['object'],
+                ],
+            close_fds=True,
+            stdout=subprocess.PIPE,
+            )
+        data = process.stdout.read()
+        returncode = process.wait()
+        if returncode != 0:
+            raise RuntimeError('git cat-file failed')
+        return data
+
+def write_object(repo, content):
+    process = subprocess.Popen(
+        args=[
+            'git',
+            '--git-dir=%s' % repo,
+            'hash-object',
+            '-w',
+            '--stdin',
+            ],
+        cwd=repo,
+        close_fds=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        )
+    process.stdin.write(content)
+    process.stdin.close()
+    sha = process.stdout.read().rstrip('\n')
+    returncode = process.wait()
+    if returncode != 0:
+        raise RuntimeError('git hash-object failed')
+    if not sha:
+        raise RuntimeError('git hash-object did not return a hash')
+    return sha
---
+++ gitfs/test/test_commands.py
@@ -0,0 +1,217 @@
+from nose.tools import (
+    eq_ as eq,
+    )
+
+from gitfs.test.util import (
+    maketemp,
+    assert_raises,
+    )
+
+import os
+
+from gitfs import commands
+
+def check_repository(repo):
+    assert os.path.isdir(repo)
+    # it's a bare repo
+    assert not os.path.exists(os.path.join(repo, '.git'))
+
+    sha = commands.rev_parse(
+        rev='refs/heads/master',
+        repo=repo,
+        )
+    assert sha is None, 'Expected no commits yet: %r' % sha
+
+    value = commands.get_symbolic_ref(repo=repo, ref='HEAD')
+    eq(value, 'refs/heads/master')
+
+def test_init():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    check_repository(repo=tmp)
+
+def test_init_repeat():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    commands.init_bare(tmp)
+    check_repository(repo=tmp)
+
+def test_symbolic_ref_ok():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    got = commands.get_symbolic_ref(repo=tmp, ref='HEAD')
+    eq(got, 'refs/heads/master')
+
+def test_symbolic_ref_bad_nonsymbolic():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    e = assert_raises(
+        # TODO better exception for this
+        RuntimeError,
+        commands.get_symbolic_ref,
+        repo=tmp,
+        ref='refs/heads/master',
+        )
+    eq(str(e), 'git symbolic-ref failed')
+
+def test_rev_parse():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    commands.fast_import(
+        repo=tmp,
+        commits=[
+            dict(
+                message='one',
+                committer='John Doe <jdoe@example.com>',
+                commit_time='1216235872 +0300',
+                files=[
+                    dict(
+                        path='foo',
+                        content='FOO',
+                        ),
+                    ],
+                ),
+            ],
+        )
+    got = commands.rev_parse(repo=tmp, rev='HEAD')
+    eq(got, 'e1b2f3253b18e7bdbd38db0cf295e6b3b608bb27')
+
+def test_ls_tree():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    commands.fast_import(
+        repo=tmp,
+        commits=[
+            dict(
+                message='one',
+                committer='John Doe <jdoe@example.com>',
+                commit_time='1216235872 +0300',
+                files=[
+                    dict(
+                        path='quux/foo',
+                        content='FOO',
+                        ),
+                    dict(
+                        path='bar',
+                        content='BAR',
+                        mode='100755',
+                        ),
+                    ],
+                ),
+            ],
+        )
+    g = commands.ls_tree(repo=tmp)
+    eq(
+        g.next(),
+        dict(
+            mode='100755',
+            type='blob',
+            object='add8373108657cb230a5379a6fcdaab73f330642',
+            name='bar',
+            ),
+        )
+    eq(
+        g.next(),
+        dict(
+            mode='040000',
+            type='tree',
+            object='d513b699a47153aad2f0cb7ea2cb9fde8c177428',
+            name='quux',
+            ),
+        )
+    assert_raises(StopIteration, g.next)
+
+def test_ls_tree_all():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    commands.fast_import(
+        repo=tmp,
+        commits=[
+            dict(
+                message='one',
+                committer='John Doe <jdoe@example.com>',
+                commit_time='1216235872 +0300',
+                files=[
+                    dict(
+                        path='quux/foo',
+                        content='FOO',
+                        ),
+                    dict(
+                        path='bar',
+                        content='BAR',
+                        mode='100755',
+                        ),
+                    ],
+                ),
+            ],
+        )
+    g = commands.ls_tree(repo=tmp, children=False)
+    eq(
+        g.next(),
+        dict(
+            mode='100755',
+            type='blob',
+            object='add8373108657cb230a5379a6fcdaab73f330642',
+            name='bar',
+            ),
+        )
+    eq(
+        g.next(),
+        dict(
+            mode='040000',
+            type='tree',
+            object='d513b699a47153aad2f0cb7ea2cb9fde8c177428',
+            name='quux',
+            ),
+        )
+    assert_raises(StopIteration, g.next)
+
+def test_cat_file_by_path():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    commands.fast_import(
+        repo=tmp,
+        commits=[
+            dict(
+                message='one',
+                committer='John Doe <jdoe@example.com>',
+                commit_time='1216235872 +0300',
+                files=[
+                    dict(
+                        path='quux/foo',
+                        content='FOO',
+                        ),
+                    ],
+                ),
+            ],
+        )
+    g = commands.cat_file_by_path(repo=tmp, path='quux/foo')
+    eq(g, 'FOO')
+
+def test_cat_file_by_path_bad_notfound():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    commands.fast_import(
+        repo=tmp,
+        commits=[
+            dict(
+                message='one',
+                committer='John Doe <jdoe@example.com>',
+                commit_time='1216235872 +0300',
+                files=[
+                    dict(
+                        path='quux/foo',
+                        content='FOO',
+                        ),
+                    ],
+                ),
+            ],
+        )
+    g = commands.cat_file_by_path(repo=tmp, path='does-not-exist')
+    assert g is None
+
+def test_write_object():
+    tmp = maketemp()
+    commands.init_bare(tmp)
+    got = commands.write_object(repo=tmp, content='FOO')
+    eq(got, 'd96c7efbfec2814ae0301ad054dc8d9fc416c9b5')
