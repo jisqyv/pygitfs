8c34bf774977be16056a65f5b901c33954e78c96
Tommi Virtanen <tv@eagain.net>
2008-12-10 18:19:01 -0800

    Add a placeholder setup.py, I need an egg of this.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name = "gitfs",
+    version = "0.1",
+    packages = find_packages(),
+
+    author = "Tommi Virtanen",
+    author_email = "tv@eagain.net",
+    description = "Filesystem-like API for Git repositories",
+    long_description = """
+
+TODO
+
+""".strip(),
+    license = "MIT",
+    keywords = "git filesystem version-control",
+    url = "http://eagain.net/software/pygitfs/",
+
+    install_requires = [
+        'fs',
+        ],
+
+    )
