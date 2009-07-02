5d60a53fb43448423876257d929d20fd750b85c2
Tommi Virtanen <tv@eagain.net>
2009-07-02 09:32:23 +0100

    Prepare for release of 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name = "gitfs",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
 
     author = "Tommi Virtanen",
@@ -12,15 +12,31 @@ setup(
     description = "Filesystem-like API for Git repositories",
     long_description = """
 
-TODO
+A pythonic filesystem API to Git, allowing you to access contents of
+bare git repositories, including creating new commits.
 
 """.strip(),
     license = "MIT",
     keywords = "git filesystem version-control",
-    url = "http://eagain.net/software/pygitfs/",
+#    url = "http://eagain.net/software/pygitfs/",
+    url = "http://eagain.net/gitweb/?p=pygitfs.git",
 
     install_requires = [
         'filesystem',
         ],
+
+    classifiers = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Operating System :: POSIX
+Operating System :: Unix
+Programming Language :: Python
+Topic :: Software Development
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Software Development :: Version Control
+Topic :: System :: Archiving
+Topic :: System :: Filesystems
+""".splitlines(),
 
     )
